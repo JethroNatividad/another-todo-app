de1e2da72738c47549dee5372ece4b1cd3a1e836
JethroNatividad <natividad.jet@gmail.com>
2024-04-08 11:19:52 +0800

    get full todos instead of paginated

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,7 +3,7 @@ import { Todo, columns } from "./columns";
 import { DataTable } from "./data-table";
 
 const getTodos = async () => {
-  const result = await db.collection("todos").getList<Todo>(1, 30);
+  const result = await db.collection("todos").getFullList<Todo>();
   return result;
 };
 
@@ -11,7 +11,7 @@ const TodosPage = async () => {
   const todos = await getTodos();
   return (
     <div>
-      <DataTable columns={columns} data={todos.items} />
+      <DataTable columns={columns} data={todos} />
     </div>
   );
 };
